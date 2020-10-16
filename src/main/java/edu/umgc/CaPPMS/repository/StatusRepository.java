8a036758e595ac8eaa3377f1406820477bf3461f
tarunlava <tarunlava@users.noreply.github.com>
2020-10-16 00:19:49 -0400

    Fixed error that comes up when the starting up app.

--- src/main/java/edu/umgc/CaPPMS/repository/StatusRepository.java
+++ src/main/java/edu/umgc/CaPPMS/repository/StatusRepository.java
@@ -15,7 +15,7 @@ import edu.umgc.CaPPMS.dto.StatusProjectResponse;
 @Repository
 public interface StatusRepository extends JpaRepository<Status, Long>{
 
-	@Query("SELECT new com.example.projecttracker.dto.StatusProjectResponse(c.status_descr , p.project_title) FROM Status c JOIN c.projects p")
+	@Query(value = "SELECT new com.example.projecttracker.dto.StatusProjectResponse(c.status_descr , p.project_title) FROM Status c JOIN c.projects p", nativeQuery = true)
 	public List<StatusProjectResponse> getJoinInformation();
 	
 	
