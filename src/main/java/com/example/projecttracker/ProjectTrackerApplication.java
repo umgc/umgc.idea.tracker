993efe141db1ed4ff2eb9e25efa695defc549712
Glenn Goodlett <ggoodlett1@student.umgc.edu>
2020-10-25 16:18:55 -0700

    fixed imports, removed empty test that causes build to fail

--- src/main/java/com/example/projecttracker/ProjectTrackerApplication.java
+++ src/main/java/com/example/projecttracker/ProjectTrackerApplication.java
@@ -2,27 +2,27 @@ package com.example.projecttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjectTrackerApplication {
 
 	@Bean
-	public WebMvcConfigurer configure()
-	{
+	public WebMvcConfigurer configure() {
 		return new WebMvcConfigurer() {
-			
-			 @Override
-			 public void addCorsMappings(CorsRegistry registry) {
-				 
-				  registry.addMapping("/**").allowedOrigins("*")
-				  .allowedMethods("GET","POST","PUT","DELETE");
-				 
-			 }
-			 
-			
-			
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+
+			}
+
 		};
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTrackerApplication.class, args);
 	}
